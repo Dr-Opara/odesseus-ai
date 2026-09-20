cc3265c9c8d3ca113a4e9196fb2ebfc6f906ac34
Emmanuel Opara (Dr.) <chimaopra@yahoo.com>
2026-09-19 22:06:00 -0500

    feat: route expanded ATS providers

--- src/lib/jobs/providers/index.ts
+++ src/lib/jobs/providers/index.ts
@@ -3,6 +3,8 @@ import { fetchAshbyJobs } from "./ashby";
 import { fetchGreenhouseJobs } from "./greenhouse";
 import { fetchLeverJobs } from "./lever";
 import { fetchWorkableJobs } from "./workable";
+import { fetchSmartRecruitersJobs } from "./smartrecruiters";
+import { fetchRecruiteeJobs } from "./recruitee";
 
 export async function fetchSourceJobs(
   source: JobSourceConfig
@@ -16,5 +18,9 @@ export async function fetchSourceJobs(
       return fetchAshbyJobs(source);
     case "workable":
       return fetchWorkableJobs(source);
+    case "smartrecruiters":
+      return fetchSmartRecruitersJobs(source);
+    case "recruitee":
+      return fetchRecruiteeJobs(source);
   }
 }
